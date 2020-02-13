40eedf4bd4f28d5ae1d4a32a7138eb9f7877e963
tylerrwatsonn <tylerjrwatson@gmail.com>
2020-02-13 16:36:41 -0500

    Initial Cucumber package and tentative step definition file

---
+++ GraphBook-Backend/src/test/java/ca/mcgill/ecse428/graphbook/cucumber/StudentSteps.java
@@ -0,0 +1,44 @@
+//package ca.mcgill.ecse428.graphbook.cucumber;
+//
+//import org.junit.runner.RunWith;
+//
+//import cucumber.api.PendingException;
+//import cucumber.api.junit.*;
+//import cucumber.api.formatter.*;
+//import cucumber.api.java.en.When;
+//import cucumber.api.junit.Cucumber;
+//
+//@RunWith(Cucumber.class)
+//public class StudentSteps {
+//
+//    @Given("^a unique (.+), (.+)$")
+//    public void a_unique_(String username, String password) throws Throwable {
+//        throw new PendingException();
+//    }
+//
+//    @Given("^an existing (.+), (.+)$")
+//    public void an_existing_(String username, String password) throws Throwable {
+//        throw new PendingException();
+//    }
+//
+//    @When("^A person requests to become a user$")
+//    public void a_person_requests_to_become_a_user() throws Throwable {
+//        throw new PendingException();
+//    }
+//
+//    @When("^a person submits a requests to become a user$")
+//    public void a_person_submits_a_requests_to_become_a_user() throws Throwable {
+//        throw new PendingException();
+//    }
+//
+//    @Then("^the persons credentials are stored and he becomes a user$")
+//    public void the_persons_credentials_are_stored_and_he_becomes_a_user() throws Throwable {
+//        throw new PendingException();
+//    }
+//
+//    @Then("^an error message is shown and person's credentials are not saved$")
+//    public void an_error_message_is_shown_and_persons_credentials_are_not_saved() throws Throwable {
+//        throw new PendingException();
+//    }
+//
+//}
