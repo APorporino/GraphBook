4869216b585b8a9d72dea145ae1ee574bcf77c40
Ege Ozer <ege.ozer@hpe.com>
2020-02-15 16:15:27 -0500

    Updated profile page Fixed all caps on login/signup

--- graph-book/src/app/profile/profile.component.ts
+++ graph-book/src/app/profile/profile.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
+  panelOpenState = false;
   constructor() { }
 
   ngOnInit(): void {
+
+
   }
 
 }
