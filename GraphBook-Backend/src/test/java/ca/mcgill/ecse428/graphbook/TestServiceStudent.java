b61d075786feff02d45f8b3c6e163817e0767e05
CharlesBourbeau <bourbeau.charles99@gmail.com>
2020-02-17 13:05:29 -0500

    All tests passing and running on JUnit 5.

--- GraphBook-Backend/src/test/java/ca/mcgill/ecse428/graphbook/TestServiceStudent.java
+++ GraphBook-Backend/src/test/java/ca/mcgill/ecse428/graphbook/TestServiceStudent.java
@@ -35,7 +35,7 @@ public class TestServiceStudent {
 	}
 
 	@BeforeEach
-	public void deleteStudents2() {
+	public void deleteStudents() {
 		service.deleteAllCourseOfferings();
 		service.deleteAllCourses();
 		service.deleteAllStudents();	
