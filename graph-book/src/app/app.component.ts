acf3d20502f7614bff2770293d19e71c93b13e3e
Ege Ozer <ege.ozer@hpe.com>
2020-02-22 16:46:23 -0500

    +Added logout-dialog.html for a dialog system on logout +Updated app.component for button and dialog for logout functionality

--- graph-book/src/app/app.component.ts
+++ graph-book/src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'graph-book';
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(LogoutDialog, {
+      width: '400px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
 }
+
+@Component({
+  selector: 'logout-dialog',
+  templateUrl: 'logout-dialog.html',
+})
+export class LogoutDialog {
+  constructor(
+      public dialogRef: MatDialogRef<LogoutDialog>,
+      @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+  //TODO: MUST TIE TO BACKEND TO ACTUALLY LOGOUT
+  logout(): void {
+    this.dialogRef.close();
+  }
+
+}
+
+
+
